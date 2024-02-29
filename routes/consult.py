ab5862a6fa5e984e10856f9f8013c8e45aab22d3
sweetmjkim <sweet.mjkim@gmail.com>
2024-02-29 18:07:37 +0900

    a

--- routes/consult.py
+++ routes/consult.py
@@ -21,3 +21,15 @@ async def list_post(request:Request):
     print(dict(await request.form()))
     return templates.TemplateResponse(name="consult/notice.html", context={'request':request})
 
+## 데이터 현황 차트
+@router.post("/data_chart") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="consult/data_chart.html", context={'request':request})
+
+@router.get("/data_chart") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="consult/data_chart.html", context={'request':request})
