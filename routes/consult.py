6dd11dca546e2c2c699433677cae369226eebbfe
sjungmin98 <shc980@gmail.com>
2024-03-04 09:21:09 +0900

    adf

--- routes/consult.py
+++ routes/consult.py
@@ -2,22 +2,27 @@ from fastapi import APIRouter
 from starlette.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+from databases.connections import Database
+from models.admin_notice import Admin_notice_list
+collection_admin_notice_list = Database(Admin_notice_list)
 
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates/")
 
-## 공지 사항
-@router.post("/notice") # 펑션 호출 방식
-async def list_post(request:Request):
-    await request.form()
-    print(dict(await request.form()))
-    return templates.TemplateResponse(name="consult/notice.html", context={'request':request})
 
-@router.get("/notice") # 펑션 호출 방식
+
+## 공지 사항
+@router.post("/user_notice") # 펑션 호출 방식
 async def list_post(request:Request):
-    await request.form()
+    notices = await collection_admin_notice_list.get_all()
     print(dict(await request.form()))
-    return templates.TemplateResponse(name="consult/notice.html", context={'request':request})
+    return templates.TemplateResponse(name="consult/user_notice.html", context={'request':request, "notices": notices})
+
+@router.get("/user_notice") # 펑션 호출 방식
+async def list_post(request:Request):
+    notices = await collection_admin_notice_list.get_all()
+    print(dict(await request.form()))
+    return templates.TemplateResponse("consult/user_notice.html" , context={"request": request, "notices": notices} )
 
