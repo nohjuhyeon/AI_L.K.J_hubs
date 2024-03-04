3e51c5f5a74f4f5c9812c76b5b625a785f1d565d
sjungmin98 <shc980@gmail.com>
2024-03-04 17:12:52 +0900

    11

---
+++ models/one_on_one_CS.py
@@ -0,0 +1,16 @@
+from typing import Optional, List
+from beanie import Document, Link
+from bson import ObjectId                 # 데이터베이스의 데이터를 문서나 링크 형태로 가져올 수 있는 기능을 제공
+# from pydantic import BaseModel, EmailStr
+
+############ 숨은 그림을 찾아보세요 ^^ ##############################################
+
+# 개발자 실수로 들어가는 field 제한
+class One_on_one_CS_list(Document):
+    _id : ObjectId
+    userName : Optional[str] = None 
+    userEmail : Optional[str] = None
+    inquiryContent : Optional[str] = None
+    date : Optional[str] = None
+    class Settings:
+        name = "one_on_one_CS"
