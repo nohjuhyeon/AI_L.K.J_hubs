a0a8c2d504bab4d2a3ac1cd1bf0e7573828dbb95
nohjuhyeon <njh2720@gmail.com>
2024-03-21 11:37:33 +0900

    asd

--- routes/event.py
+++ routes/event.py
@@ -6,6 +6,8 @@ from fastapi.staticfiles import StaticFiles
 from databases.connections import Database
 from models.attraction_search_info import attraction_search_info
 from models.season_concept_info import season_concept_info
+from models.data_chart import data_concept_search
+collection_data_concept_search=Database(data_concept_search)
 collection_attraction = Database(attraction_search_info)
 collection_season_concept = Database(season_concept_info)
 
@@ -24,6 +26,19 @@ async def list_post(request:Request):
 async def list_post(request:Request):
     await request.form()
     print(dict(request._query_params))
+    dict_region={'서울':0, '경기':0, '인천':0, '강원':0, '경남':0, '경북':0, '광주':0, '대구':0, '대전':0,
+                   '부산':0, '세종':0, '울산':0, '전남':0, '전북':0, '충남':0, '충북':0, '제주':0}
+    try:
+        std_month = int(dict(request._query_params)['month'])
+    except:
+        std_month = 3
+    visitor_conditions = {"std_month" : std_month,"destination_type" : { '$regex': '전체'}}
+    visitor_dict = await collection_data_concept_search.getsbyconditions(visitor_conditions)
+    visitor_dict = [module.dict() for module in visitor_dict]
+    for i in range(len(visitor_dict)):
+        dict_region[visitor_dict[i]['region']] = dict_region[visitor_dict[i]['region']] + visitor_dict[i]['destination_search']
+    dict_region = dict(sorted(dict_region.items(), key=lambda x: x[1], reverse=True))
+    region_list = list(dict_region.keys())[:5]
     return templates.TemplateResponse(name="event/best_region.html", context={'request':request})
 
 ## 관광지 추천
